e8609365f1b37bb3ca0fa3ed02ae8911cd759a66
Denis <d.khokhloff2011@yandex.ru>
2022-09-06 11:48:07 +0300

    filters are removed and cleaned

--- src/components/JobPosition.jsx
+++ src/components/JobPosition.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+
 import { Badge } from 'UI/Badge';
 import { Card } from 'UI/Card';
 import { Stack } from 'UI/Stack';
@@ -17,6 +18,7 @@ const JobPosition = ({
 	location,
 	languages,
 	tools,
+	handleAddFilter,
 }) => {
   const badges = [].concat(role, level, ...languages, ...tools);
 
@@ -65,7 +67,10 @@ const JobPosition = ({
 			</div>
 			<Stack>
 				{badges.map(item => (
-				<Badge key={item}>{item}</Badge>
+					<Badge 
+						key={item}
+						onClick={() => handleAddFilter(item)}>{item}
+					</Badge>
 				))}
 			</Stack>
 			</div>
@@ -89,4 +94,5 @@ JobPosition.propTypes = {
 	location: PropTypes.string,
 	languages: PropTypes.arrayOf(PropTypes.string),
 	tools: PropTypes.arrayOf(PropTypes.string),
+	handleAddFilter: PropTypes.func,
 };
--- src/store/positions/position-selectors.js
+++ src/store/positions/position-selectors.js
@@ -5,11 +5,7 @@ export const selectVisiblePositions = (state, filters = []) => {
         return state.positions;
     }
     return state.positions.filter(pos => {
-        const posFilters = [].concat(
-            pos.role,
-            pos.level,
-            ...pos.languages,
-            ...pos.tools);
+        const posFilters = [].concat(pos.role, pos.level, ...pos.languages, ...pos.tools);
         
         return filters.every(filter => posFilters.includes(filter))
     });
